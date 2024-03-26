fd869c9a93892918f3581fd56cd1cef573daae26
Dhiraj <dhiraj@pop-os.localdomain>
2024-03-27 00:18:25 +0530

    Added venv selector

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,7 +6,24 @@ local M = {}
 
 M.ui = {
 	-- transparency = true,
-	statusline = { theme = "vscode_colored", modules = { "swenv" } },
+	statusline = {
+		theme = "default",
+		separator_style = "round",
+    overriden_modules = nil,
+    modules = {
+      venv = function ()
+        local venv = require('venv-selector').get_active_venv()
+        if venv then
+          local venv_parts = vim.fn.split(venv, '/')
+          local venv_name = venv_parts[#venv_parts]
+          return venv_name
+        else
+          return 'Select Venv'
+        end
+      end,
+    }
+	},
+
 	theme = "oxocarbon",
 	theme_toggle = { "chadracula-evondev", "oxocarbon" },
 	telescope = { style = "bordered" },
